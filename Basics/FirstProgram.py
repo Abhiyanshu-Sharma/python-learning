472190e03494963716440ed7663c532a52625055
Abhiyanshu-Sharma <abhiyanshusharma010305@gmail.com>
2026-08-24 21:04:50 +0530

    Initialising python repository

---
+++ Basics/FirstProgram.py
@@ -0,0 +1,4 @@
+# This is my First Python Program
+
+print("Hello World with double quotes")
+print('Hello world with single quotes')
---
+++ Basics/Variables.py
@@ -0,0 +1,5 @@
+first_name = "Abhiyanshu"
+food = "pizza"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
