dc1af96d5b182f41bea60657078ee56dfa6d1f7a
Abhiyanshu-Sharma <abhiyanshusharma010305@gmail.com>
2026-09-17 21:50:49 +0530

    Nested Loops

---
+++ 12_Loops/Nested/Rectangle.py
@@ -0,0 +1,8 @@
+rows = int(input("Enter the # of Rows: "))
+columns = int(input("Enter the # of Columns: "))
+symbol = input("Enter a symbol to use: ")
+
+for x in range(rows):
+    for y in range(columns):
+        print(symbol, end = "")
+    print()
